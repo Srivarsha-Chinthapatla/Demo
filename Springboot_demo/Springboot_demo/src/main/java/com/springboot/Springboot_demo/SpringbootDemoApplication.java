8c6d5e61fcea839ddb5d4001ba38b5901ba6f237
Srivarsha Chinthapatla <C.Srivarsha@ivycomptech.com>
2022-03-21 12:54:22 +0530

    commit

---
+++ Springboot_demo/Springboot_demo/src/main/java/com/springboot/Springboot_demo/SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.Springboot_demo;
+import java.util.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+
+ System.out.println("hi..springboot");}
+}
